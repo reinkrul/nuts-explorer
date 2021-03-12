356cc52fc025746b5c3b3361fd4869af3af3a973
Rein Krul <info@reinkrul.nl>
2021-03-12 19:20:44 +0100

    Fixed listing updated DIDs

--- gateway.go
+++ gateway.go
@@ -29,28 +29,38 @@ func (g serviceProxy) ListDIDs(w http.ResponseWriter) error {
 	if err := json.Unmarshal(transactionsAsBytes, &transactions); err != nil {
 		return err
 	}
-	var result []string
+	resultsAsMap := make(map[string]bool, 0)
+	var results []string
 	for _, str := range transactions {
-		if  tx, err := jws.ParseString(str); err != nil {
+		if tx, err := jws.ParseString(str); err != nil {
 			return err
 		} else {
 			hdrs := tx.Signatures()[0].ProtectedHeaders()
 			if hdrs.ContentType() == "application/did+json" {
 				// This is a DID. Derive DID from JWK key ID.
-				if keyID, err := did.ParseDID(hdrs.JWK().KeyID()); err != nil {
-					return err
+				var keyID *did.DID
+				if hdrs.JWK() != nil {
+					if keyID, err = did.ParseDID(hdrs.JWK().KeyID()); err != nil {
+						return err
+					}
 				} else {
-					curr := *keyID
-					curr.Fragment = ""
-					result = append(result, curr.String())
+					if keyID, err = did.ParseDID(hdrs.KeyID()); err != nil {
+						return err
+					}
 				}
-
+				curr := *keyID
+				curr.Fragment = ""
+				// Updates end up as duplicate entries
+				if !resultsAsMap[curr.String()] {
+					results = append(results, curr.String())
+				}
+				resultsAsMap[curr.String()] = true
 			}
 		}
 	}
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(result)
+	data, _ := json.Marshal(results)
 	_, err = w.Write(data)
 	return err
 }
