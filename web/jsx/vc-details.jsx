3a9fa8df3204cb824f86103c335570bc9213c560
Rein Krul <info@reinkrul.nl>
2021-04-06 17:00:11 +0200

    Fixed VC details ID

--- web/jsx/vc-details.jsx
+++ web/jsx/vc-details.jsx
@@ -1,7 +1,7 @@
 class VCDetails extends React.Component {
     render() {
         return <div>
-            <h2>VC: <code>{this.props.id}</code></h2>
+            <h2>VC: <code>{this.props.document.verifiableCredential.id}</code></h2>
             <pre><code>{JSON.stringify(this.props.document, null, 2)}</code></pre>
         </div>
     }
